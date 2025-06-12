e2307cddc2b9631017bf6802f727622d2c3e6958
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-06-12 14:36:42 +0530

    Initial Upload of epub Before starting changes to it

---
+++ 9780443408151/OEBPS/widgets/Poptip/script/poptip.js
@@ -0,0 +1,4 @@
+var glossaries = {
+ fn1Ch008: `<p><sup>*</sup>Harding, M., Kwong, J., Roberts, D., Hagler, D., &#38; Reinisch, C. (2020). <em>Lewis&#8217;s medical-surgical nursing: Assessment and management of clinical problems</em> (11th ed.). Elsevier.</p>`,
+
+}
