0a4c991c89b5950cc6e5bf54d324bbbe68c11f89
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-06-19 23:10:12 +0530

    Epub Check errors resolutions.

---
+++ 9780443350108/OEBPS/widgets/quiz_json/Appendix_E_UCS1_MCMS.js
@@ -0,0 +1,23 @@
+var quiz = [
+{
+        q: `Select four findings that require immediate follow-up.`,
+        q2: ``,
+        q3: [],
+        option: [` Pain level of 2/10 on a 0–10 scale after SL NTG`,` Apical pulse is bounding`,`BP: 154/92 mmHg`,`Shortness of breath with exertion`,` SpO2 96% on room air`,
+            `Complains of dizziness and light-headed`,`Weight gain of 25 pounds (11.4 kg) over the last 2 months`,`Potassium level is 4.7 mmol/L (Ref. range = 3.5–5 mEq/L or 3.5–5 mmol/L)`],
+        //optionFeedback: [``],
+        optionStyleType: `st-decimal`,
+        answer: [2,3,5,6],
+        ansText:`Rationale: Hypertension is a risk factor for CAD. An elevated blood pressure (BP) increases cardiac workload which can cause ischemia to the heart 
+        muscle in clients with significant atherosclerosis. Shortness of breath with exertion is another sign of increased heart workload and decreased cardiac muscle
+        perfusion. Dizziness and light-headedness could result from decreased blood flow from narrowing of the arteries supplying oxygen to the brain. New onset or worsening
+        of these symptoms could indicate a neurological complication and require further assessment. A weight gain of 25 pounds (11.4 kg) in a two-month period is significant and 
+        can put excessive strain on the heart. This weight gain could also indicate that the client may be experiencing other complications such as congestive heart failure or kidney injury.
+        The other findings do not require immediate follow-up.`, 
+        state: `notAnswered`,
+        userAnswered: [],
+        type:`MCMS`
+    }
+
+    
+];
---
+++ 9780443350108/OEBPS/widgets/quiz_json/Appendix_E_UCS2_MCMS.js
@@ -0,0 +1,22 @@
+var quiz = [
+
+	
+	{
+        q: `After reinforcing discharge teaching, the PN assesses the client’s understanding. Which statement(s) indicate adequate understanding of the discharge teaching? `,
+        q2: ``,
+        q3: [],
+        option: [`“Swelling and redness in my right leg is to be expected and I should elevate it when this occurs.”`,`“My pain meds may cause me to be constipated so I should eat oatmeal every day for breakfast.”`,
+        ` “I should use an electric razor as long as I am taking the blood thinners.”`,` “I will be able to drive myself to the rehab center.”`,` “My wife will need to go fill my prescriptions at the pharmacy and take them to the rehab center, or I will not have any medicines to take.”`],
+        optionFeedback: [`This is not an appropriate action. This finding could indicate a deep vein thrombosis is present. The client should notify the PN immediately.`,`Taking opioids may lead to constipation. Adding whole grains such as oatmeal will add fiber to the diet and decrease the risk of constipation.`,
+        `Using an electric razor is advised when clients are prescribed anticoagulants to reduce the risk of bleeding.`,`It is not advised for the client to drive while taking opioids. Transportation should be provided to the facility.`,
+         `The rehabilitation facility will receive the client's prescriptions before discharge. The facility is responsible for obtaining the needed medications for the client. This is usually not a responsibility of the family or client.`],
+        optionStyleType: `st-decimal`,
+        answer: [1,2],
+        ansText:`Rationale: Taking opioids may lead to constipation. Adding whole grains such as oatmeal will add fiber to the diet and decrease the risk of constipation.,
+        Using an electric razor is advised when clients are prescribed anticoagulants to reduce the risk of bleeding. `, 
+        state: `notAnswered`,
+        userAnswered: [],
+        type:`MCMS`
+    } 
+
+];
