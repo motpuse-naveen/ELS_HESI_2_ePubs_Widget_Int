a6aaf8cf2db7658aa4c29024667007ce18d396cb
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-06-22 19:09:40 +0530

    Some changes to the Inline question and dropdown design.

--- 9780443350108/OEBPS/widgets/inline_questions/data/ch04_question_data.js
+++ 9780443350108/OEBPS/widgets/inline_questions/data/ch04_question_data.js
@@ -45,7 +45,7 @@ const questions_data = {
           dropdowns: {
             dropdown1: {
               values: [],
-              correctValue: "4",
+              correctValue: "1",
               dropdownlabel: "Select the Procedures",
               useCommonOptions: "true",
             },
@@ -58,7 +58,7 @@ const questions_data = {
           dropdowns: {
             dropdown1: {
               values: [],
-              correctValue:"1",
+              correctValue:"6",
               dropdownlabel: "Select the Procedures",
               useCommonOptions: "true",
             },
@@ -71,7 +71,7 @@ const questions_data = {
           dropdowns: {
             dropdown1: {
               values: [],
-              correctValue:"6",
+              correctValue:"7",
               dropdownlabel: "Select the Procedures",
               useCommonOptions: "true",
             },
@@ -84,7 +84,7 @@ const questions_data = {
           dropdowns: {
             dropdown1: {
               values: [],
-              correctValue:"7",
+              correctValue:"5",
               dropdownlabel: "Select the Procedures",
               useCommonOptions: "true",
             },
@@ -97,7 +97,7 @@ const questions_data = {
           dropdowns: {
             dropdown1: {
               values: [],
-              correctValue:"5",
+              correctValue:"3",
               dropdownlabel: "Select the Procedures",
               useCommonOptions: "true",
             },
@@ -105,9 +105,76 @@ const questions_data = {
           dropdownPlacement: "inline", //inline
         }
       ],
-      itemsInstruction: `<p>* Lymph nodes are collections of tissue containing white blood cells called lymphocytes.</p>
-                      <p>† A squamous cell is a type of epithelial cell.</p>`,
-      correctFeedback: `All of your answers are correct.`,
+      /*itemsInstruction: `<p>* Lymph nodes are collections of tissue containing white blood cells called lymphocytes.</p>
+                      <p>† A squamous cell is a type of epithelial cell.</p>`,*/
+      correctFeedback: `<p>All of your answers are correct.</p><p><strong>Rationale: </strong>First determine the client’s level of responsiveness and breathing adequacy, and if a pulse is present. Then initiate a call for help by activating the code team. Then move the client to the floor to begin high-quality chest compressions. You deliver 30 compressions followed by 2 breaths. Once the defibrillator arrives, you will check the rhythm using the quick-look paddles.</p>`,
+      partialFeedback: `You have one or more incorrect answers.`,
+      incorrectFeedback: `Your answers are incorrect.`,
+      optionStyleType: "st-decimal",
+      itemSeperator: "false",
+    },
+    group2: {
+      id: "group2",
+      type: "dropdown",
+      placeholder_id: "group2_placeholder",
+      commonDropdownOptions: ["1", "2", "3", "4"],
+      items: [
+        {
+          headerImage: {},
+          dropdownHtml: `<span>A 70-year-old complaining of a pain level of 8/10 from a hand burn #dropdown1#</span>`,
+          dropdowns: {
+            dropdown1: {
+              values: [],
+              correctValue:"2",
+              dropdownlabel: "Select the Procedures",
+              useCommonOptions: "true",
+            },
+          },
+          dropdownPlacement: "inline", //inline
+        },
+        {
+            headerImage: {},
+            dropdownHtml: `<span>A 35-year-old with partial- and full-thickness burns to the anterior and posterior chest #dropdown1#</span>`,
+            dropdowns: {
+              dropdown1: {
+                values: [],
+                correctValue: "4",
+                dropdownlabel: "Select the Procedures",
+                useCommonOptions: "true",
+              },
+            },
+            dropdownPlacement: "inline", //inline
+          },
+        {
+          headerImage: {},
+          dropdownHtml: `<span>A 25-year-old with a superficial burn to the right anterior arm and lateral chest #dropdown1#</span>`,
+          dropdowns: {
+            dropdown1: {
+              values: [],
+              correctValue: "1",
+              dropdownlabel: "Select the Procedures",
+              useCommonOptions: "true",
+            },
+          },
+          dropdownPlacement: "inline", //inline
+        },
+        {
+          headerImage: {},
+          dropdownHtml: `<span>A 42-year-old with a partial-thickness burn to the anterior lower extremity and confusion #dropdown1#</span>`,
+          dropdowns: {
+            dropdown1: {
+              values: [],
+              correctValue:"3",
+              dropdownlabel: "Select the Procedures",
+              useCommonOptions: "true",
+            },
+          },
+          dropdownPlacement: "inline", //inline
+        },
+      ],
+      /*itemsInstruction: `<p>* Lymph nodes are collections of tissue containing white blood cells called lymphocytes.</p>
+                      <p>† A squamous cell is a type of epithelial cell.</p>`,*/
+      correctFeedback: `<p>All of your answers are correct.</p><p><strong>Rationale:</strong> The client with partial- and full-thickness burns to the chest is at greatest risk for respiratory issues and should be assessed first to ensure airway and breathing are maintained. The client with partial-thickness burns to the lower extremities and is confused shows signs of possible deterioration and should be assessed next. The older client should be seen next, and the younger client with superficial burns can be seen last in this group.</p>`,
       partialFeedback: `You have one or more incorrect answers.`,
       incorrectFeedback: `Your answers are incorrect.`,
       optionStyleType: "st-decimal",
