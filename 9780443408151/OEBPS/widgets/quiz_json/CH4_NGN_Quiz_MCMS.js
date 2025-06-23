e8c23821132143c8d5553730171eef65196417f0
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-06-23 13:07:21 +0530

    some changes.

---
+++ 9780443408151/OEBPS/widgets/quiz_json/CH4_NGN_Quiz_MCMS.js
@@ -0,0 +1,17 @@
+var quiz = [
+{
+        q: `<p><strong>Scenario:</strong> The RN is caring for a client who has a new prescription for warfarin 2 mg PO daily. The RN is providing medication education for the client and is discussing dietary instructions including foods to avoid while taking this medication.</p>
+        <p>Which vegetable would the RN instruct this client to avoid while taking warfarin? (Select all that apply.)</p>`,
+        q2: ``,
+        q3: [],
+        option: [`Collard greens`, `Iceberg lettuce`, `Broccoli`, `Brussels sprouts`, `Green beans`, `Spinach`, `Zucchini squash`,`Kale`],
+        optionFeedback: [``],
+        optionStyleType: `st-decimal`,
+        answer: [0,2,3,5,7],
+        ansText:`<p><strong>Rationale:</strong> Collard greens, broccoli, Brussels sprouts, spinach, and kale are foods that are vitamin K–rich and should be avoided while taking warfarin and other anticoagulants. Iceberg lettuce, green beans, and zucchini squash are lower in vitamin K and are good substitutes for these vegetables for a well-balanced diet.</p>`, 
+        state: `notAnswered`,
+        userAnswered: [],
+        type:`MCMS`
+    },
+
+];
